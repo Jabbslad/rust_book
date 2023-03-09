2cab4cfa6b5452caa79c8e16cd37f845c6d6633c
Jamie Atkinson <jabbslad@gmail.com>
2023-03-09 00:49:15 +0000

    added comparison to guessing game

--- ch2/ch2-guessing-game/src/main.rs
+++ ch2/ch2-guessing-game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("the secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,4 +18,12 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    let guess = guess.trim().parse::<u32>().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
