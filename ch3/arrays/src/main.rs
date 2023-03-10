5d8a84b195742f5256ab695293ffef2af2125c59
Jamie Atkinson <jabbslad@gmail.com>
2023-03-10 23:56:04 +0000

    add ability to read from stdin and access array

--- ch3/arrays/src/main.rs
+++ ch3/arrays/src/main.rs
@@ -1,4 +1,16 @@
+use std::io::stdin;
+
 fn main() {
     let a = [1, 2, 3, 4, 5];
     println!("{:?}", a);
+
+    let b: Vec<u8> = (1..=10).collect();
+
+    println!("enter an index [0->9]: ");
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("need to enter valid index");
+    let val: usize = buf.trim().parse().expect("failed to parse index");
+
+    println!("{:?}", b[val]);
+
 }
