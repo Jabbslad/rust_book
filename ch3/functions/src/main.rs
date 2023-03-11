0bd819ef9d9316d1dc90c61a33df8eeda986a7da
Jamie Atkinson <jabbslad@gmail.com>
2023-03-11 22:31:35 +0000

    add return types to functions

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     let unit = 'h';
     print_labeled_measurement(5, &unit);
     println!("unit = {unit}");
+
+    println!("The value of x is: {}", five());
+
+    println!("5 plus 1 = {}", plus_one(5));
 }
 
 fn another_function() {
@@ -18,4 +22,12 @@ fn another_function2(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: &char) {
     println!("The measurement is {value}{unit_label}");
+}
+
+fn five() -> u8 {
+    5
+}
+
+fn plus_one(x: u8) -> u8 {
+    x + 1
 }
