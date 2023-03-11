df618a196440f73c3b9ad41926aeccf8e4c2e3e8
Jamie Atkinson <jabbslad@gmail.com>
2023-03-11 22:18:41 +0000

    add function that takes parameters by reference

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
 
     another_function();
     another_function2(32);
+    let unit = 'h';
+    print_labeled_measurement(5, &unit);
+    println!("unit = {unit}");
 }
 
 fn another_function() {
@@ -11,4 +14,8 @@ fn another_function() {
 
 fn another_function2(x: i32) {
     println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: &char) {
+    println!("The measurement is {value}{unit_label}");
 }
