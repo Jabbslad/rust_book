1193f4e2686016ad50af7392a967bfadc1892436
Jamie Atkinson <jabbslad@gmail.com>
2023-03-11 22:06:53 +0000

    add parameterized function

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_function2(32);
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn another_function2(x: i32) {
+    println!("The value of x is: {}", x);
 }
