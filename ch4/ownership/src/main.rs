bc478490585cea6393bc36865a850a0bc72ec9ad
Jamie Atkinson <jabbslad@gmail.com>
2023-03-13 21:58:36 +0000

    compare two arrays on stack (copy)

--- ch4/ownership/src/main.rs
+++ ch4/ownership/src/main.rs
@@ -1,3 +1,15 @@
+fn read(y: bool) {
+    if y {
+        println!("y is true");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = true;
+    read(x);
+
+    let a = [1, 2, 3, 4, 5];
+    let b = a;
+
+    assert!(std::ptr::eq(&a, &b));
 }
